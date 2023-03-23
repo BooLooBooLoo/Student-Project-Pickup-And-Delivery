e87f2d5acc30d355f40fdb75628d8b75e5fef314
BLSquared <92587994+BooLooBooLoo@users.noreply.github.com>
2023-03-23 15:48:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
 
 class Problem :
-    def __init__(self, drivers,driversCoord,points,pointsCoord, rides,dist):
-        distBtwPoints = []
+    def __init__(self, drivers,driversCoord,points,pointsCoord, rides):
+        dist = 0
         self.drivers = drivers
         self.driversCoord = driversCoord
         self.points = points
@@ -12,9 +12,8 @@ class Problem :
             ind1 = self.points.index(point1)
             ind2 = self.points.index(point2)
             dist = math.sqrt((pointsCoord[ind1][0]-pointsCoord[ind2][0])**2 + (pointsCoord[ind1][1]-pointsCoord[ind2][1])**2)
-            distBtwPoints.append()
-        
+            return dist
 
-prob = Problem([0,1,2],['Casa','Marrakech','Tanger'],['Casa','Marrakech','Tanger'],[("Marrakech","Tanger"),("Casa","Marrakech")])
 
-print(prob.rides[0])
+prob = Problem([0,1,2],[(23,23),(90,1),(1,2)],["Home","Work","School"],[(2,2),(9,19),(17,92)],[("Home","Work"),("Work","School"),("School","Home")])
+
