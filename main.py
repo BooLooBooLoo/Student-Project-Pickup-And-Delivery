efec43f2dfca85c4d4651d7e2f9c36303d37aa0b
BLSquared <92587994+BooLooBooLoo@users.noreply.github.com>
2023-03-28 13:49:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,21 @@ prob = Problem([0,1,2],[(23,23),(90,1),(1,2)],["Home","Work","School"],[(2,2),(9
 
 class Solution :
     
-    problem = prob
     
-    def __init__(self, driversRides):
+    def __init__(self, driversRides, prob):
         self.driversRides = driversRides # list of tuples (driver,[rides])
+        self.problem = prob
+        self.ObjectiveFunction = 0
         
-    def DistanceBetweenPoints(self,problem,point1, point2):
-            ind1 = problem.points.index(point1)
-            ind2 = problem.points.index(point2)
-            dist = math.sqrt((problem.pointsCoord[ind1][0]-problem.pointsCoord[ind2][0])**2 + 
-                             (problem.pointsCoord[ind1][1]-problem.pointsCoord[ind2][1])**2)
+    def DistanceBetweenPoints(self,point1, point2):
+            ind1 = self.problem.points.index(point1)
+            ind2 = self.problem.points.index(point2)
+            dist = math.sqrt((self.problem.pointsCoord[ind1][0]-self.problem.pointsCoord[ind2][0])**2 + 
+                             (self.problem.pointsCoord[ind1][1]-self.problem.pointsCoord[ind2][1])**2)
             return dist
     
-    def Feasability(self,driver,ride):
-        #To check later
+    def Feasability(self):
+        #Also check if all the rides are dispatch and if the ride isnt already assigned to a driver
         if driver in self.problem.drivers and ride in self.problem.rides:
             return True
         else:
@@ -43,5 +44,5 @@ class Solution :
         return 0
 
 
-
+#Do the class method to construct a solution and check if it is feasible
 
