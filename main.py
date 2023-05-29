6def59c4494f44da151e3deb457e60ac0c814ebe
EwenMailly <90908620+EwenMailly@users.noreply.github.com>
2023-05-29 21:00:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import math
 import random
-#import data
+<<<<<<< HEAD
+from data import get_data
+import pandas as pd
+import numpy as np
+import datetime
+import pytz
+import openrouteservice
+=======
+import data
 
 
+>>>>>>> parent of c8f0183 (divers files)
 class Problem :
     
     def __init__(self, drivers,driversCoord,speed,cost,points,pointsCoord, rides):
@@ -17,8 +26,13 @@ class Problem :
         self.points = points
         self.pointsCoord = pointsCoord
         self.rides = rides
+<<<<<<< HEAD
+data=get_data(datetime.datetime(2023, 1, 5, 0, 0, 0, 0,pytz.UTC),datetime.datetime(2023, 1, 5, 0, 30, 0, 0,pytz.UTC),0)
+prob = Problem(data[3],data[4],50,1,data[0],data[1],data[2])        
+=======
         
 prob = Problem([1,2,3],[(0,0),(0,0),(0,0)],1,1,['Home','School','Work'],[(2,2),(1,1),(3,3)],[('Home','School'),('Work','Home'),('School','Work')])
+>>>>>>> parent of c8f0183 (divers files)
 
 class Solution :
 
@@ -30,9 +44,15 @@ class Solution :
     def DistanceBetweenPoints(self,point1, point2):
             ind1 = self.problem.points.index(point1)
             ind2 = self.problem.points.index(point2)
-            dist = math.sqrt((self.problem.pointsCoord[ind1][0]-self.problem.pointsCoord[ind2][0])**2 + 
-                             (self.problem.pointsCoord[ind1][1]-self.problem.pointsCoord[ind2][1])**2)
-            return dist
+            lat_1 = self.problem.pointsCoord[ind1][0]
+            lng_1 = self.problem.pointsCoord[ind1][1]
+            lat_2 = self.problem.pointsCoord[ind2][0]
+            lng_2 = self.problem.pointsCoord[ind2][1]
+            lat_1, lng_1, lat_2, lng_2 = map(math.radians, [lat_1, lng_1, lat_2, lng_2])
+            d_lat = lat_2 - lat_1
+            d_lng = lng_2 - lng_1
+            temp=(math.sin(d_lat / 2) ** 2 + math.cos(lat_1) * math.cos(lat_2) * math.sin(d_lng / 2) ** 2)
+            return 6373.0 * (2 * math.atan2(math.sqrt(temp), math.sqrt(1 - temp)))
     
     def Feasability(self, prob):
         #Also check if all the rides are dispatch and if the ride isnt already assigned to a driver
@@ -51,15 +71,15 @@ class Solution :
                 rides.append(self.driversRides[i][1][j])
         for i in range (0,len(self.problem.rides)):
             if self.problem.rides[i] not in rides:
+                print("ride not dispatched")
                 feasible = False
         #Check if the rides are not assigned to multiple drivers
         for i in range (0,len(self.driversRides)):
             for j in range (0,len(self.driversRides[i][1])):
                 for k in range (0,len(self.driversRides)):
-                    if i != k:
-                        for l in range (0,len(self.driversRides[k][1])):
-                            if self.driversRides[i][1][j] == self.driversRides[k][1][l]:
-                                feasible = False
+                    if k != i:
+                        if self.driversRides[i][1][j] in self.driversRides[k][1]:
+                            feasible = False
         
         return feasible
         
@@ -69,8 +89,17 @@ class Solution :
         for i in range (0,len(driversRides)):
             if len(driversRides[i][1]) != 0:
                 ind = self.problem.points.index(driversRides[i][1][0][0])
-                distance = math.sqrt((self.problem.driversCoord[i][0]-self.problem.pointsCoord[ind][0])**2 + 
-                                (self.problem.driversCoord[i][1]-self.problem.pointsCoord[ind][1])**2)
+                lat_1 = self.problem.pointsCoord[ind][1]
+                lng_1 = self.problem.pointsCoord[ind][0]
+                lat_2 = self.problem.driversCoord[i][0]
+                lng_2 = self.problem.driversCoord[i][1]
+                print(lat_1,lng_1,lat_2,lng_2)
+                lat_1, lng_1, lat_2, lng_2 = map(math.radians, [lat_1, lng_1, lat_2, lng_2])
+                d_lat = lat_2 - lat_1
+                d_lng = lng_2 - lng_1
+                temp=(math.sin(d_lat / 2) ** 2 + math.cos(lat_1) * math.cos(lat_2) * math.sin(d_lng / 2) ** 2)
+                distance = 6373.0 * (2 * math.atan2(math.sqrt(temp), math.sqrt(1 - temp)))
+                print(distance)
                 self.objective += distance
                 for j in range (0,len(driversRides[i][1])):
                     self.objective += self.DistanceBetweenPoints(driversRides[i][1][j][0],driversRides[i][1][j][1]) * self.problem.driversCost
@@ -240,12 +269,31 @@ class Method :
                     if new_solution_cost<best_solution_cost:
                         best_solution=new_solution
                         best_solution_cost=new_solution_cost
-            #If delta>0, replace the initial solution with the new solution with a probability according to the temperature and the delta
-            elif random.uniform(0.01,0.99)<math.exp(-delta/temp):
-                ini_solution=new_solution
-            temp=temp*alpha
+                #If delta>0, replace the initial solution with the new solution with a probability according to the temperature and the delta
+                elif random.uniform(0.01,0.99)<math.exp(-delta/temp):
+                    ini_solution=new_solution
+                temp=temp*alpha
         return best_solution, best_solution_cost
 #try the simulated annealing algorithm with prob
 method=Method(prob)
+<<<<<<< HEAD
+best_solution, best_solution_cost=method.SimulatedAnnealing(100, 100, 0.99)
+client = openrouteservice.Client(key='5b3ce3597851110001cf6248953dea1a63794872a30093a3907dfb03') # Specify your personal API key
+print(best_solution_cost)
+for i in range(len(best_solution.driversRides)):
+    if len(best_solution.driversRides[i][1])>0:
+        print(best_solution.driversRides[i])
+#Use the API to calculate the time between the points
+#for i in range(len(best_solution.driversRides)):
+#    if len(best_solution.driversRides[i][1])>0:
+#        coords=[]
+#        coords.append(prob.driversCoord[i])
+#        for j in range(len(best_solution.driversRides[i][1])):
+#            coords.append(prob.pointsCoord[prob.points.index(best_solution.driversRides[i][1][j][0])])
+#            coords.append(prob.pointsCoord[prob.points.index(best_solution.driversRides[i][1][j][1])])
+#        routes = client.directions(coords)
+#        print(routes['routes'][0]['summary']['duration'])
+=======
 sol=method.SimulatedAnnealing(1000,100,0.99)
 print(sol[0].driversRides,sol[1])
+>>>>>>> parent of c8f0183 (divers files)
